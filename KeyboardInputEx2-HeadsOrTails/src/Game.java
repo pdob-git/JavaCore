9340fcf1bddc5413294b891a52ea9331f6dd7445
Pawel <synk@poczta.onet.pl>
2021-11-01 01:02:46 +0100

    Add exercise for Keyboard input - scanner. KeyboardInputEx2-HeadsOrTails
    Add exercise for Keyboard input - scanner. KeyboardInputEx2-HeadsOrTails
    Exercise name:
        KeyboardInputEx2-HeadsOrTails

---
+++ KeyboardInputEx2-HeadsOrTails/src/Game.java
@@ -0,0 +1,9 @@
+class Game {
+    public static void main(String[] args) {
+        HeadsOrTails headsOrTails = new HeadsOrTails();
+        headsOrTails.betResult();
+        headsOrTails.flipCoin();
+        headsOrTails.showResult();
+    }
+}
+
