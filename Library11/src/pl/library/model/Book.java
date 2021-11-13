cd90a2f7af6226bb65eaa06c0b4b1f0e645109b2
Pawel <synk@poczta.onet.pl>
2021-11-13 14:03:46 +0100

    Add exercise for Inheritance lesson. Library11
    Add exercise for Inheritance lesson. Update library app to Library11
    Exercise name:
        Library11

---
+++ Library11/src/pl/library/model/Book.java
@@ -0,0 +1,61 @@
+package pl.library.model;
+
+public class Book  extends Publication {
+    //Fields
+    private String author;
+    private int pages;
+    private String isbn;
+    //Constructors
+
+    public Book(String title, String author, int year, int pages, String publisher,
+                String isbn)  {
+        setTitle(title);
+        setPublisher(publisher);
+        setYear(year);
+
+
+        this.pages = pages;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+
+    public void printInfo() {
+        String info = getTitle() + "; " + author + "; " + getYear() + "; "
+                + pages + "; " + getPublisher();
+        if (isbn != null){
+            info = info + "; " + isbn;
+        }
+        System.out.println(info);
+    }
+
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
