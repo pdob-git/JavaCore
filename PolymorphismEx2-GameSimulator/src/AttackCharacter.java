439a5c39c2bd0a5a211430f251154c473b3b488d
Pawel <synk@poczta.onet.pl>
2021-11-13 23:15:38 +0100

    Add exercise for Polymorphism lesson. PolymorphismEx2-GameSimulator
    Add exercise for Polymorphism lesson. PolymorphismEx2-GameSimulator
    Exercise name:
        PolymorphismEx2-GameSimulator

---
+++ PolymorphismEx2-GameSimulator/src/AttackCharacter.java
@@ -0,0 +1,21 @@
+class AttackCharacter extends GameCharacter {
+    private double extraAttack;
+
+    public AttackCharacter(String name, int energy, int attack, int defense, double extraAttack) {
+        super(name, energy, attack, defense);
+        this.extraAttack = extraAttack;
+    }
+
+    @Override
+    double totalAttack() {
+        return getAttack() + extraAttack * getAttack();
+    }
+
+    public double getExtraAttack() {
+        return extraAttack;
+    }
+
+    public void setExtraAttack(double extraAttack) {
+        this.extraAttack = extraAttack;
+    }
+}
