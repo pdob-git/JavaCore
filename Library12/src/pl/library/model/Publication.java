40a3415c03b7e6fd0b9eb5529133cdcce571e547
Pawel <synk@poczta.onet.pl>
2021-11-13 21:01:28 +0100

    Add exercise for Super lesson. Library12
    Add exercise for Super lesson. Update library app to Library12
    Exercise name:
        Library12

---
+++ Library12/src/pl/library/model/Publication.java
@@ -0,0 +1,37 @@
+package pl.library.model;
+
+public class Publication {
+    private int year;
+    private String title;
+    private String publisher;
+
+    public Publication(String title, String publisher, int year) {
+        this.title = title;
+        this.publisher = publisher;
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+}
