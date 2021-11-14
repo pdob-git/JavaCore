b79ffe7852ab36045bc596363dc4fe6aa57b05cc
Pawel <synk@poczta.onet.pl>
2021-11-14 21:44:34 +0100

    Add exercise for Polymorphism lesson. Library13
    Add exercise for Polymorphism lesson. Update library app to Library13
    Exercise name:
        Library13

---
+++ Library13/src/pl/library/model/Book.java
@@ -0,0 +1,55 @@
+package pl.library.model;
+
+public class Book extends Publication {
+    //Fields
+    private String author;
+    private int pages;
+    private String isbn;
+    //Constructors
+
+    public Book(String title, String author, int year, int pages, String publisher,
+                String isbn) {
+        super(title, publisher, year);
+
+        this.pages = pages;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    @Override
+    public void printInfo() {
+        String info = getTitle() + "; " + author + "; " + getYear() + "; "
+                + pages + "; " + getPublisher();
+        if (isbn != null) {
+            info = info + "; " + isbn;
+        }
+        System.out.println(info);
+    }
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
---
+++ Library13/src/pl/library/model/Library.java
@@ -0,0 +1,55 @@
+package pl.library.model;
+
+public class Library {
+
+
+    private static final int MAX_PUBLICATIONS = 2000;
+    private int publicationsNumber = 0;
+    private Publication[] publications = new Publication[MAX_PUBLICATIONS];
+
+    public void addBook(Book book) {
+        if (publicationsNumber < MAX_PUBLICATIONS) {
+          publications[publicationsNumber] = book;
+            publicationsNumber++;
+        } else {
+            System.out.println("Maksymalna liczba książek została osiągnięta");
+        }
+
+    }
+
+    public void printBooks() {
+        int countBooks = 0;
+        for (int i = 0; i < publicationsNumber; i++) {
+            if (publications[i] instanceof Book){
+                publications[i].printInfo();
+                countBooks++;
+            }
+        }
+        if (countBooks == 0) {
+            System.out.println("Brak książek w bibliotece");
+        }
+    }
+
+    public void addMagazine(Magazine magazine) {
+        if(publicationsNumber < MAX_PUBLICATIONS) {
+            publications[publicationsNumber] = magazine;
+            publicationsNumber++;
+        } else {
+            System.out.println("Maksymalna liczba magazynów została osiągnięta");
+        }
+
+    }
+
+    public void printMagazines() {
+        int countMagazines = 0;
+        for (int i = 0; i < publicationsNumber; i++) {
+            if (publications[i] instanceof Magazine){
+                publications[i].printInfo();
+                countMagazines++;
+            }
+        }
+        if (countMagazines == 0) {
+            System.out.println("Brak magazynów w bibliotece");
+        }
+    }
+}
