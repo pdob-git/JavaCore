f2bfa5029d4ec2ad908f36cab38187ada48c0b67
Pawel <synk@poczta.onet.pl>
2021-11-28 22:18:40 +0100

    Add exercise for Interfaces Lesson. InterfacesEx2-Company
    Add exercise for Interfaces Lesson. InterfacesEx2-Company
        InterfacesEx2-Company

---
+++ InterfacesEx2-Company/src/Employee.java
@@ -0,0 +1,37 @@
+abstract class Employee {
+    public static final int NO_OF_MONTHS = 12;
+    private String firstName;
+    private String lastName;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName
+                + ", wypłata miesięczna: " + getMonthlySalary()
+                + ", wypłata roczna: " + getYearlySalary();
+    }
+
+    abstract double getMonthlySalary();
+
+    abstract double getYearlySalary();
+}
