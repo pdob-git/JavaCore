203ce2275db5db7eb6a1bb4d15109e0d4c810a96
Pawel <synk@poczta.onet.pl>
2021-11-29 18:08:49 +0100

    Add exercise for Interfaces Lesson. Library16 modified
    Add exercise for Interfaces Lesson. Update library app to Library16.
    Publication class changed to abstract.
    Exercise name:
        Library16

--- Library16/src/pl/library/model/Publication.java
+++ Library16/src/pl/library/model/Publication.java
@@ -2,7 +2,7 @@ package pl.library.model;
 
 import java.util.Objects;
 
-public class Publication {
+public abstract class Publication {
     private String title;
     private String publisher;
     private int year;
