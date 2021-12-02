8214c56520f6f7dce28ab311dfa96ce6edb97658
Pawel <synk@poczta.onet.pl>
2021-12-02 21:55:58 +0100

    Add exercise for InnerClass Lesson. Library17
    Add exercise for InnerClass Lesson. Update library app to Library17.
    enum Option moved to LibraryControl as inner enum
    Exercise name:
        Library17

---
+++ Library17/src/pl/library/model/Library.java
@@ -0,0 +1,32 @@
+package pl.library.model;
+
+public class Library {
+
+    private static final int MAX_PUBLICATIONS = 2000;
+    private int publicationsNumber;
+    private Publication[] publications = new Publication[MAX_PUBLICATIONS];
+
+    public Publication[] getPublications() {
+        Publication[] result = new Publication[publicationsNumber];
+        for (int i = 0; i < publicationsNumber; i++) {
+            result[i] = publications[i];
+        }
+        return result;
+    }
+
+    public void addBook(Book book) {
+        addPublication(book);
+    }
+
+    public void addMagazine(Magazine magazine) {
+        addPublication(magazine);
+    }
+
+    private void addPublication(Publication publication) {
+        if (publicationsNumber >= MAX_PUBLICATIONS) {
+            throw new ArrayIndexOutOfBoundsException("Max publications exceeded " + MAX_PUBLICATIONS);
+        }
+        publications[publicationsNumber] = publication;
+        publicationsNumber++;
+    }
+}
