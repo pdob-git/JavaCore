09a716eb92fd505e081b36fc92a6b0ea1af9468d
Pawel <synk@poczta.onet.pl>
2021-12-04 21:05:34 +0100

    Add exercise for Files Lesson. Files-Serialization
    Add exercise for Files Lesson. Files-Serialization
    Exercise from serialization lesson.
    Exercise name:
        Files-Serialization

---
+++ Files-Serialization/src/ObjectWriter.java
@@ -0,0 +1,22 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+class ObjectWriter {
+    public static void main(String[] args) {
+        String fileName = "person.obj";
+        Person p1 = new Person("Jan", "Kowalski");
+
+        try(
+                var fs = new FileOutputStream(fileName);
+                var os = new ObjectOutputStream(fs);
+        ) {
+            os.writeObject(p1);
+            System.out.println("Zapisano obiekt do pliku");
+
+        } catch (IOException e) {
+            System.err.println("Bład zapisu pliku " + fileName);
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Files-Serialization/src/Person.java
@@ -0,0 +1,30 @@
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private static final long serialVersionUID = 3812017177088226528L;
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Person(String firstName, String lastName) {
+        setFirstName(firstName);
+        setLastName(lastName);
+    }
+
+}
