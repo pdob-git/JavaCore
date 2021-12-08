ffa59683f5994a46e494b3cac503412710cd35f3
Pawel <synk@poczta.onet.pl>
2021-12-08 14:38:33 +0100

    Add exercise for InnerClass Lesson. Library18
    Add exercise for InnerClass Lesson. Update library app to Library18.
    Save to files serialized files added.
    Exercise name:
        Library18

---
+++ Library18/src/pl/library/io/ConsolePrinter.java
@@ -0,0 +1,37 @@
+package pl.library.io;
+
+import pl.library.model.Book;
+import pl.library.model.Magazine;
+import pl.library.model.Publication;
+
+public class ConsolePrinter {
+    public void printBooks(Publication[] publications) {
+        int counter = 0;
+        for (Publication publication : publications) {
+            if(publication instanceof Book) {
+                printLine(publication.toString());
+                counter++;
+            }
+        }
+        if (counter == 0)
+            printLine("Brak książek w bibliotece");
+    }
+
+    public void printMagazines(Publication[] publications) {
+        int counter = 0;
+        for (Publication publication : publications) {
+            if(publication instanceof Magazine) {
+                printLine(publication.toString());
+                counter++;
+            }
+        }
+        if (counter == 0)
+            printLine("Brak magazynów w bibliotece");
+    }
+
+    public void printLine(String text) {
+
+//        System.out.println(text.toUpperCase());
+        System.out.println(text);
+    }
+}
