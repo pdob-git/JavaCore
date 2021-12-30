3afcf0c137d58dc05929d512ed7a3ccc95f6dfbf
Pawel <synk@poczta.onet.pl>
2021-12-30 22:11:24 +0100

    Add exercise for Maps Lesson. Library22
    Add exercise for Maps Lesson. Update library app to Library22.
    Change CsvFileManager, by implementing CsvConvertible interface
    in User and Publication classes.
    Exercise name:
        Library22

---
+++ Library22/src/pl/library/model/Library.java
@@ -0,0 +1,56 @@
+package pl.library.model;
+
+
+import pl.library.exception.PublicationAlreadyExistsException;
+import pl.library.exception.UserAlreadyExistsException;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Library implements Serializable {
+    private static final long  SerialVersionUID = -7838941;
+
+
+    //zmieniony typ
+    private Map<String, Publication> publications = new HashMap<>();
+    //dodane
+    private Map<String, LibraryUser> users = new HashMap<>();
+
+    public Map<String, Publication> getPublications() {
+        return publications;
+    }
+
+    public Map<String, LibraryUser> getUsers() {
+        return users;
+    }
+
+    //dodana metoda i rzucany nowy typ wyjątku
+    public void addUser(LibraryUser user) {
+        if(users.containsKey(user.getPesel()))
+            throw new UserAlreadyExistsException(
+                    "Użytkownik ze wskazanym peselem już istnieje " + user.getPesel()
+            );
+        users.put(user.getPesel(), user);
+    }
+
+    //zmieniona logika
+    public void addPublication(Publication publication) {
+        if(publications.containsKey(publication.getTitle()))
+            throw new PublicationAlreadyExistsException(
+                    "Publikacja o takim tytule już istnieje " + publication.getTitle()
+            );
+        publications.put(publication.getTitle(), publication);
+    }
+
+    //zmieniona logika
+    public boolean removePublication(Publication publication) {
+        if(publications.containsValue(publication)) {
+            publications.remove(publication.getTitle());
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
