e777a64d0a63d06f75e12633a24beddc1ef7b42d
Pawel <synk@poczta.onet.pl>
2022-01-01 20:06:44 +0100

    Add exercise for Queue Lesson. QueueEx1-TaskManager
    Add exercise for Queue Lesson. QueueEx1-TaskManager
    Application to manage tasks using Priority Queue
    Exercise name:
        QueueEx1-TaskManager

---
+++ QueueEx1-TaskManager/src/page/TaskManagerApp.java
@@ -0,0 +1,8 @@
+package page;
+
+class TaskManagerApp {
+    public static void main(String[] args) {
+        TaskManager taskManager = new TaskManager();
+        taskManager.mainLoop();
+    }
+}
