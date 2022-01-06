f1a0e1bdf46491cf9a9c6526ee905dcf858e3e89
Pawel <synk@poczta.onet.pl>
2022-01-06 21:42:57 +0100

    Add exercise for MethodReference Lesson. Library25
    Add exercise for MethodReferenceLesson. Update library app to Library25.
    Replace lambdas with methods reference.
    Exercise name:
        Library25

---
+++ Library25/src/pl/library/io/file/FileManagerBuilder.java
@@ -0,0 +1,51 @@
+package pl.library.io.file;
+
+import pl.library.exception.NoSuchFileTypeException;
+import pl.library.io.ConsolePrinter;
+import pl.library.io.DataReader;
+
+public class FileManagerBuilder {
+    private ConsolePrinter printer;
+    private DataReader reader;
+
+    public FileManagerBuilder(ConsolePrinter printer, DataReader reader) {
+        this.printer = printer;
+        this.reader = reader;
+    }
+
+    public FileManager build() {
+        printer.printLine("Wybierz format danych:");
+        FileType fileType = getFileType();
+        switch (fileType) {
+            case CSV:
+                return new CsvFileManager();
+            case SERIAL:
+                return new SerializableFileManager();
+            default:
+                throw new NoSuchFileTypeException("Nieobsługiwany typ danych");
+        }
+    }
+
+    private FileType getFileType() {
+        boolean typeOk = false;
+        FileType result = null;
+        do {
+            printTypes();
+            String type = reader.getString().toUpperCase();
+            try {
+                result = FileType.valueOf(type);
+                typeOk = true;
+            } catch (IllegalArgumentException e) {
+                printer.printLine("Nieobsługiwany typ danych, wybierz ponownie.");
+            }
+        } while (!typeOk);
+
+        return result;
+    }
+
+    private void printTypes() {
+        for (FileType value : FileType.values()) {
+            printer.printLine(value.name());
+        }
+    }
+}
