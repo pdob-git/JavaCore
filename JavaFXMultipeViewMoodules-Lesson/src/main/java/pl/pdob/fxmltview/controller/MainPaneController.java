c184867912e0d8067086392e386cd4b62915e7bd
Pawel <synk@poczta.onet.pl>
2022-01-28 23:39:01 +0100

    JavaFX Multiple Modules Views lesson. JavaFXMultipeViewMoodules-Lesson
    JavaFX Events Lessons. JavaFXMultipeViewMoodules-Lesson
    Lesson showing how to combine view from multiple fxml files.
    Exercise:
        JavaFXMultipeViewMoodules-Lesson

---
+++ JavaFXMultipeViewMoodules-Lesson/src/main/java/pl/pdob/fxmltview/controller/MainPaneController.java
@@ -0,0 +1,20 @@
+package pl.pdob.fxmltview.controller;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainPaneController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+}
