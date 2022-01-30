71e3e7885d38d030a4006efcb10a654119cc3c3c
Pawel <synk@poczta.onet.pl>
2022-01-30 21:37:14 +0100

    JavaFX Multiple Modules Views lesson. JavaFX-Mp3Player
    JavaFX Multiple Modules Views lesson. JavaFX-Mp3Player
    Add declarations of child planes in main controller.
    Exercise:
        JavaFX-Mp3Player

--- JavaFX-Mp3Player/src/main/java/pl/pdob/mp3player/controller/MainController.java
+++ JavaFX-Mp3Player/src/main/java/pl/pdob/mp3player/controller/MainController.java
@@ -1,14 +1,25 @@
 package pl.pdob.mp3player.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+    @FXML
+    private ContentPaneController contentPaneController;
+    @FXML
+    private ControlPaneController controlPaneController;
+    @FXML
+    private MenuPaneController menuPaneController;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("Main controller created");
+        System.out.println(contentPaneController);
+        System.out.println(controlPaneController);
+        System.out.println(menuPaneController);
     }
 }
