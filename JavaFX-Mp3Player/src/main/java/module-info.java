97df69aab1c8b90814832c30708d254772db99fb
Pawel <synk@poczta.onet.pl>
2022-02-04 22:27:15 +0100

    JavaFX External Libraries lesson. JavaFX-Mp3Player
    JavaFX External Libraries lesson. JavaFX-Mp3Player
    Add test table view data for mp3 song.
    Exercise:
        JavaFX-Mp3Player

--- JavaFX-Mp3Player/src/main/java/module-info.java
+++ JavaFX-Mp3Player/src/main/java/module-info.java
@@ -2,7 +2,9 @@ module mp3player {
     requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires jid3lib;
 
     exports pl.pdob.mp3player.main to javafx.graphics;
     opens pl.pdob.mp3player.controller to javafx.fxml;
+    opens pl.pdob.mp3player.mp3 to javafx.base;
 }
--- JavaFX-Mp3Player/src/main/java/pl/pdob/mp3player/controller/ContentPaneController.java
+++ JavaFX-Mp3Player/src/main/java/pl/pdob/mp3player/controller/ContentPaneController.java
@@ -1,19 +1,55 @@
 package pl.pdob.mp3player.controller;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import pl.pdob.mp3player.mp3.Mp3Song;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ContentPaneController implements Initializable {
+
+    private static final String TITLE_COLUMN = "Tytuł";
+    private static final String AUTHOR_COLUMN = "Autor";
+    private static final String ALBUM_COLUMN = "Album";
+
     @FXML
-    private TableView<?> contentTable;
+    private TableView<Mp3Song> contentTable;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("Content controller created");
+        configureTableColumns();
+        createTestData();
+    }
+
+
+
+    private void configureTableColumns() {
+        TableColumn<Mp3Song, String> titleColumn = new TableColumn<Mp3Song, String>(TITLE_COLUMN);
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+
+        TableColumn<Mp3Song, String> authorColumn = new TableColumn<Mp3Song, String>(AUTHOR_COLUMN);
+        authorColumn.setCellValueFactory(new PropertyValueFactory<>("author"));
+
+        TableColumn<Mp3Song, String> albumColumn = new TableColumn<Mp3Song, String>(ALBUM_COLUMN);
+        albumColumn.setCellValueFactory(new PropertyValueFactory<>("album"));
+
+        contentTable.getColumns().add(titleColumn);
+        contentTable.getColumns().add(authorColumn);
+        contentTable.getColumns().add(albumColumn);
+    }
+
+    private void createTestData() {
+        ObservableList<Mp3Song> items = contentTable.getItems();
+        items.add(new Mp3Song("a", "a", "a", "a"));
+        items.add(new Mp3Song("b", "b", "b", "b"));
+        items.add(new Mp3Song("c", "c", "c", "c"));
+        items.add(new Mp3Song("d", "d", "d", "d"));
+
     }
 }
